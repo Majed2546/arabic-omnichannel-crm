272cc79693b82cb07a3dba2ce411b7a9ff7b0779
Majed2546 <majed.h.mutairi@gmail.com>
2026-05-26 16:57:38 +0300

    Fix automation module tenant access dependency

--- backend/src/modules/automation/automation.module.ts
+++ backend/src/modules/automation/automation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { CommonModule } from '../../common/common.module'
 import { AutomationController } from './automation.controller'
 import { AutomationExecutorService } from './automation-executor.service'
 import { AutomationService } from './automation.service'
 
 @Module({
+  imports: [CommonModule],
   controllers: [AutomationController],
   providers: [AutomationService, AutomationExecutorService],
   exports: [AutomationService, AutomationExecutorService],
